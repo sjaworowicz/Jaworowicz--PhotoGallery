6f682f3c922b0b6caafce28149e09c5715aeea9f
sjaworowicz <125sjaworowicz@frhsd.com>
2025-01-08 09:45:04 -0500

    Object Constructor to Create Objects

--- js/gallery.js
+++ js/gallery.js
@@ -54,7 +54,7 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = 'images.json';
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -73,16 +73,32 @@ $(document).ready( function() {
 	
 });
 
+function iterateJSON() {
+	mJson.forEach(GalleryImage(mImages));
+};
+
+function fetchJSON() {
+
+mRequest.onreadystatechange = function(){
+	if(this.readyState == 4 && this.status == 200)
+	mJson = JSON.parse(mRequest.responseText);
+	iterateJSON();
+};
+mRequest.open("GET", mUrl, true);
+mRequest.send();
+	}
+
+
+
 window.addEventListener('load', function() {
 	
 	console.log('window loaded');
 
 }, false);
 
-function GalleryImage() {
-	//implement me as an object to hold the following data about an image:
-	//1. location where photo was taken
-	//2. description of photo
-	//3. the date when the photo was taken
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+function GalleryImage(location, description, date, img) {
+	this.location = location;
+	this.description = description;
+	this.date = date;
+	this.img = img;
 }
